d5d801db8e8f6fcf5f8ca162d806fe4aa677ba49
giffy onyinye <giffyonyinye@gmail.com>
2024-08-16 17:11:25 +0100

    Add project files.

---
+++ Task1Api/FetchDogBreed.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Task1Api
+{
+    public class FetchDogBreed
+    {
+        public async Task FetchAllDogBreeds()
+        {
+            string folderPath = "C:\\Users\\onyinyechi.odiegwu\\source\\repos\\Task1Api\\Task1Api";
+            string fileName = "dog_breeds.txt";
+            string fullPath = folderPath + fileName;
+            string url = "https://dog.ceo/api/breeds/list/all";
+           try
+            {
+                using (HttpClient client =new HttpClient())
+                {
+                   /* client.DefaultRequestHeaders.Add("x-rapidapi-key", "dbb99f5902msh4b77a1f0fe49d35p1421b2jsn3983bffc1940");
+                    client.DefaultRequestHeaders.Add("x-rapidapi-host", "movie-database-alternative.p.rapidapi.com");*/
+
+                    using (HttpResponseMessage response = await client.GetAsync(url))
+                    {
+                        using (HttpContent content = response.Content)
+                        {
+                            string data = content.ReadAsStringAsync().Result;
+                            if (data != null)
+                            {
+                                var dataObj = JObject.Parse(data);
+                                using (StreamWriter writer = new StreamWriter(fullPath))
+                                {
+                                    writer.WriteLine($"Log Time: {DateTime.Now}");
+                                    foreach (var breed in dataObj)
+                                    {
+                                        writer.WriteLine(breed.ToString());
+                                    }
+                                    writer.WriteLine(new string('-', 20));
+                                }
+                                Console.WriteLine(dataObj);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Data is null!");
+                            }
+                        }
+                    }
+                }
+
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Task1Api/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+
+namespace Task1Api
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var dogBreeds = new FetchDogBreed();
+            while (true)
+            {
+                await dogBreeds.FetchAllDogBreeds();
+                //Thread.Sleep(7200000);
+                Thread.Sleep(1000);
+
+            }
+        }
+    }
+}
+
